9b452a6c132f9beac2c76207af49dfc543683ff1
bescribanobegue <b.escribano.begue@accenture.com>
2024-03-19 15:36:50 +0100

    ++

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+description = "Kotlin version of the Spring Petclinic application"
+group = "org.springframework.samples"
+// Align with Spring Version
+version = "3.2.2"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+plugins {
+    val kotlinVersion = "1.9.20"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.google.cloud.tools.jib") version "3.1.4"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+}
+
+// WebJars versions are also referenced in src/main/resources/templates/fragments/layout.html for resource URLs
+val boostrapVersion = "5.1.3"
+val fontAwesomeVersion = "4.7.0"
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.glassfish.jaxb:jaxb-runtime")
+    implementation("javax.cache:cache-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.webjars.npm:bootstrap:$boostrapVersion")
+    implementation("org.webjars.npm:font-awesome:$fontAwesomeVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+jib {
+    to {
+        image = "springcommunity/spring-petclinic-kotlin"
+        tags = setOf(project.version.toString(), "latest")
+    }
+}
+
